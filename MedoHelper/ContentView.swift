ada3f41c07237d3ffb4a58008693faa95c5aee62
Rafael Schmitt <rafael.claycon@gmail.com>
2022-05-24 23:17:50 -0300

    Initial Commit

---
+++ MedoHelper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MedoHelper
+//
+//  Created by Rafael Schmitt on 24/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MedoHelper/MedoHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  MedoHelperApp.swift
+//  MedoHelper
+//
+//  Created by Rafael Schmitt on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MedoHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
