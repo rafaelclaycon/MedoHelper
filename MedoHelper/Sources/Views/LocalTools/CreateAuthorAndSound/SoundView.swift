6c662ad0b3de8d750b848dd4a9e2260c499411d9
Rafael Schmitt <rafael.claycon@gmail.com>
2023-06-04 23:44:39 -0300

    Fix duration having to include file inside project

--- MedoHelper/Sources/Views/LocalTools/CreateAuthorAndSound/SoundView.swift
+++ MedoHelper/Sources/Views/LocalTools/CreateAuthorAndSound/SoundView.swift
@@ -3,45 +3,74 @@ import SwiftUI
 struct SoundView: View {
     
     @State var sound = ProtoSound(title: "", description: "", filename: "", dateAdded: Date(), isOffensive: false, successMessage: "...")
+    @State private var showFilePicker = false
+    @State private var selectedFile: URL? = nil
+    
+    // Alert
+    @State private var showingAlert = false
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    
+    private var filename: String {
+        return selectedFile?.lastPathComponent ?? ""
+    }
+
+    private var hasAllNecessaryData: Bool {
+        return sound.title != "" && sound.description != "" && selectedFile != nil
+    }
     
     var body: some View {
-        VStack {            
+        VStack(spacing: 30) {
             TextField("Título do Som", text: $sound.title)
-                .padding()
             
             TextField("Descrição do Som", text: $sound.description)
-                .padding()
             
-            TextField("Nome do arquivo (sem .mp3)", text: $sound.filename)
-                .padding()
+            HStack(spacing: 30) {
+                Button("Selecionar arquivo...") {
+                    showFilePicker = true
+                }
+                .fileImporter(isPresented: $showFilePicker, allowedContentTypes: [.mp3]) { result in
+                    do {
+                        selectedFile = try result.get()
+                        print(selectedFile as Any)
+                    } catch {
+                        alertTitle = "Erro ao Selecionar Arquivo"
+                        alertMessage = error.localizedDescription
+                        showingAlert = true
+                    }
+                }
+                .alert(isPresented: $showingAlert) {
+                    Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("OK")))
+                }
+                
+                Text(filename)
+            }
             
             DatePicker("Data de adição", selection: $sound.dateAdded, displayedComponents: .date)
                 .datePickerStyle(.compact)
                 .labelsHidden()
-                .padding()
             
             Toggle("É ofensivo", isOn: $sound.isOffensive)
-                .padding()
             
-            Button("Copiar JSON Som") {
-                let pasteboard = NSPasteboard.general
-                pasteboard.clearContents()
-                pasteboard.setString(generateSoundJSON(), forType: .string)
-                sound.successMessage = "JSON de '\(sound.title)' copiado!"
-            }
-
-            Text(sound.successMessage)
-                .padding()
-            
-            Button("Calcular duração") {
+            Button("Copiar JSON do Som") {
                 Task {
-                    await print(FileHelper.getDuration(of: "\(sound.filename).mp3") ?? "")
+                    guard let soundJSONString = await createSoundJSON() else { return }
+                    
+                    let pasteboard = NSPasteboard.general
+                    pasteboard.clearContents()
+                    pasteboard.setString(soundJSONString, forType: .string)
+                    
+                    alertTitle = "JSON do Som Copiado com Sucesso"
+                    alertMessage = ""
+                    showingAlert = true
                 }
             }
+            .disabled(!hasAllNecessaryData)
         }
+        .padding()
     }
     
-    func generateSoundJSON() -> String {
+    private func createSoundJSON() async -> String? {
         let soundId = UUID().uuidString
         
         var dateString = ""
@@ -53,7 +82,12 @@ struct SoundView: View {
             dateString = "\(year)-\(formattedMonth)-\(formattedDay)"
         }
         
-        return ",\n{\n\t\"id\": \"\(soundId)\",\n\t\"title\": \"\(sound.title)\",\n\t\"authorId\": \"\(authorId)\",\n\t\"description\": \"\(sound.description)\",\n\t\"filename\": \"\(sound.filename).mp3\",\n\t\"dateAdded\": \"\(dateString)T00:00:00Z\",\n\t\"duration\": 0.0,\n\t\"isOffensive\": \(sound.isOffensive ? "true": "false")\n}"
+        guard let fileURL = selectedFile else { return nil }
+        guard let duration = await FileHelper.getDuration(of: fileURL) else { return nil }
+        
+        let formattedDuration = String(format: "%.2f", duration)
+        
+        return ",\n{\n\t\"id\": \"\(soundId)\",\n\t\"title\": \"\(sound.title)\",\n\t\"authorId\": \"\(authorId)\",\n\t\"description\": \"\(sound.description)\",\n\t\"filename\": \"\(filename)\",\n\t\"dateAdded\": \"\(dateString)T00:00:00Z\",\n\t\"duration\": \(formattedDuration),\n\t\"isOffensive\": \(sound.isOffensive ? "true": "false")\n}"
     }
 }
 
