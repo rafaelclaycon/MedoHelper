ff6e0d7d1c465ab877f15c98bdb85d1c4d2176ea
Rafael Schmitt <rafael.claycon@gmail.com>
2024-01-22 16:53:27 -0300

    Update server URL

--- MedoHelper/Sources/AppSupport/MedoHelperApp.swift
+++ MedoHelper/Sources/AppSupport/MedoHelperApp.swift
@@ -7,8 +7,8 @@
 
 import SwiftUI
 
-public let serverPath: String = CommandLine.arguments.contains("-USE_LOCAL_SERVER") ? "http://127.0.0.1:8080/api/" : "http://medodelirioios.lat:8080/api/"
 public let assetOperationPassword = "total-real-password-3"
+public let serverPath: String = CommandLine.arguments.contains("-USE_LOCAL_SERVER") ? "http://127.0.0.1:8080/api/" : "http://medodelirioios.online:8080/api/"
 
 @main
 struct MedoHelperApp: App {
