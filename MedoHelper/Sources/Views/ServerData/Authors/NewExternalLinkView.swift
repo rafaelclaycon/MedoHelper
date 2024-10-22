96a070f898b0449a2f53e01d758a18a192f695a5
Rafael Schmitt <rafael.claycon@gmail.com>
2024-10-22 18:49:16 -0300

    Add pre-defined services for external links on Authors

--- MedoHelper/Sources/Views/ServerData/Authors/NewExternalLinkView.swift
+++ MedoHelper/Sources/Views/ServerData/Authors/NewExternalLinkView.swift
@@ -13,6 +13,8 @@ struct NewExternalLinkView: View {
 
     @State private var externalLink: ExternalLink
     @State private var showRemoveAlert: Bool = false
+    @State private var selectedService: MediaService.ID?
+    @State private var usualServices: [MediaService] = [.instagram, .pocketCasts, .spotify, .twitch, .youTube]
 
     // MARK: - Private Variables
 
@@ -33,15 +35,22 @@ struct NewExternalLinkView: View {
     // MARK: - Initializers
 
     init(
-        externalLink: ExternalLink?,
+        externalLink: ExternalLink,
         saveAction: @escaping (ExternalLink) -> Void,
         removeAction: @escaping (ExternalLink) -> Void
     ) {
-        self.isEditing = externalLink != nil
-        self.externalLink = externalLink ?? ExternalLink()
+        self.isEditing = externalLink.title != ""
+        self.externalLink = externalLink
         self.saveAction = saveAction
         self.removeAction = removeAction
+
+        if externalLink.title == "" {
+            self.selectedService = MediaService.youTube.id
+            onSelectedServiceChange(newValue: MediaService.youTube.id)
+        }
     }
+
+    // MARK: - View Body
 
     var body: some View {
         VStack(spacing: 30) {
@@ -53,11 +62,23 @@ struct NewExternalLinkView: View {
                 Spacer()
             }
 
-            TextField("Título", text: $externalLink.title)
+            Picker("Tipo: ", selection: $selectedService) {
+                ForEach(usualServices) { service in
+                    Text(service.name).tag(Optional(service.id))
+                }
+                Text("<Personalizado>").tag(nil as Author.ID?)
+            }
+            .onChange(of: selectedService) { _, newValue in
+                onSelectedServiceChange(newValue: newValue)
+            }
 
-            TextField("Símbolo", text: $externalLink.symbol)
+            if selectedService == nil {
+                TextField("Título", text: $externalLink.title)
 
-            TextField("Cor", text: $externalLink.color)
+                TextField("Símbolo", text: $externalLink.symbol)
+
+                TextField("Cor", text: $externalLink.color)
+            }
 
             TextField("URL", text: $externalLink.link)
 
@@ -107,11 +128,104 @@ struct NewExternalLinkView: View {
         }
         .padding(.all, 26)
     }
+
+    // MARK: - Functions
+
+    private func onSelectedServiceChange(newValue: String?) {
+        if
+            let newValue,
+            let service = MediaService(id: newValue)
+        {
+            externalLink.title = service.name
+            externalLink.symbol = service.symbol
+            externalLink.color = service.color
+        } else {
+            externalLink.title = ""
+            externalLink.symbol = ""
+            externalLink.color = ""
+        }
+    }
 }
+
+// MARK: - Internal Data
+
+extension NewExternalLinkView {
+
+    enum MediaService: Identifiable {
+
+        case instagram, pocketCasts, spotify, twitch, youTube
+
+        var id: String {
+            self.name
+        }
+
+        var name: String {
+            switch self {
+            case .instagram:
+                return "Instagram"
+            case .pocketCasts:
+                return "Pocket Casts"
+            case .spotify:
+                return "Spotify"
+            case .twitch:
+                return "Twitch"
+            case .youTube:
+                return "YouTube"
+            }
+        }
+
+        var symbol: String {
+            switch self {
+            case .instagram:
+                return "instagram-orange.png"
+            case .pocketCasts:
+                return "pocket-casts.png"
+            case .spotify:
+                return "spotify.png"
+            case .twitch:
+                return "twitch.png"
+            case .youTube:
+                return "youtube-full-color.png"
+            }
+        }
+
+        var color: String {
+            switch self {
+            case .instagram:
+                return "orange"
+            case .pocketCasts, .youTube:
+                return "red"
+            case .spotify:
+                return "green"
+            case .twitch:
+                return "purple"
+            }
+        }
+
+        init?(id: String) {
+            switch id {
+            case "Instagram":
+                self = .instagram
+            case "Pocket Casts":
+                self = .pocketCasts
+            case "Spotify":
+                self = .spotify
+            case "Twitch":
+                self = .twitch
+            case "YouTube":
+                self = .youTube
+            default:
+                return nil
+            }
+        }
+    }
+}
+
+// MARK: - Preview
 
 #Preview {
     NewExternalLinkView(
-        externalLink: nil,
+        externalLink: .init(),
         saveAction: { _ in },
         removeAction: { _ in }
     )
